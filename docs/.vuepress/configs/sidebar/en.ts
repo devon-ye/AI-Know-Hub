f3faa7ab573b7156cc081db85d2d3fc1837c7ccc
devon <devon.ye@foxmail.com>
2024-01-31 12:34:12 +0800

    初始化提交

---
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -0,0 +1,71 @@
+import type { SidebarConfig } from '@vuepress/theme-default'
+
+export const sidebarEn: SidebarConfig = {
+  '/theoretical-foundations/': [
+    {
+      text: 'Theoretical Foundations',
+      children: [
+        '/en/theoretical-foundations/mathematical-foundations.md',
+        '/en/theoretical-foundations/computational-models.md',
+        '/en/theoretical-foundations/cognitive-science-basics.md',
+      ],
+    },
+  ],
+  '/core-technologies': [
+    {
+      text: 'Core Technologies',
+      children: [
+        '/en/core-technologies/machine-learning/introduction.md',
+        '/en/core-technologies/machine-learning/supervised-learning.md',
+        '/en/core-technologies/machine-learning/unsupervised-learning.md',
+        '/en/core-technologies/machine-learning/reinforcement-learning.md',
+      ],
+    },
+    {
+      text: 'Deep learning',
+      children: [
+
+      ],
+    },
+  ],
+  '/application-domains/': [
+    {
+      text: 'NLP',
+      collapsible: true,
+      children: [
+
+      ],
+    },
+    {
+      text: 'CV',
+      collapsible: true,
+      children: [
+
+      ],
+    },
+    {
+      text: 'Robotics',
+      collapsible: true,
+      children: [
+
+      ],
+    },
+    {
+      text: 'Engineering Practice',
+      collapsible: true,
+      children: [
+        {
+          text: 'Model Training',
+          children: [
+          ],
+        },
+        {
+          text: 'Model Evaluation',
+          children: [
+
+          ],
+        }
+      ],
+    },
+  ],
+}
